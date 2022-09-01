88037be31f03cd7277735cb92b60406f32014e5b
LeoMbm <leonidas.j.mbm@gmail.com>
2022-09-01 14:16:44 +0200

    update

---
+++ js/__tests___/exo3.test.js
@@ -0,0 +1,9 @@
+// const app = require('../exo3')
+//
+//
+// describe('String without use string', () => {
+//     it('should return Hello Nalios!', () => {
+//         expect(app.stringWithoutString()).toEqual("Hello, Nalios !");
+//     })
+//
+// })
